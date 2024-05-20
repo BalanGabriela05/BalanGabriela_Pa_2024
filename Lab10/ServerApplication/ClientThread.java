91d6ff763f752ef59ebb642793ea6d65b6e1d075
BalanGabriela05 <gabry.balan05@yahoo.com>
2024-05-20 23:40:36 +0300

    lab10

--- Lab10/ServerApplication/ClientThread.java
+++ Lab10/ServerApplication/ClientThread.java
@@ -1,40 +1,296 @@
 package org.example;
 
+import org.example.Exception.GameException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ClientThread extends Thread {
+    //constante
+    public static final int CARRIER_LENGTH = 5;
+    public static final int BATTLESHIP_LENGTH = 4;
+    public static final int DESTROYER_LENGTH = 3;
+    public static final int SUBMARINE_LENGTH = 3;
+    public static final int PATROL_BOAT_LENGTH = 2;
+
+
     private final Socket clientSocket;
+    private PrintWriter out;
+    private ClientThread opponent;
+
     private final GameServer gameServer;
+    private static GameState playerTurn;
+    private int playerId;
+    private boolean shipsPlaced;
+    private boolean moveSubmitted;
+
+
+    //private AtomicInteger state;
+
 
     public ClientThread(Socket clientSocket, GameServer gameServer) {
         this.clientSocket = clientSocket;
         this.gameServer = gameServer;
-    }
+        this.playerId = gameServer.getNumberOfPlayers().get(); //l am refacut/
 
+        this.shipsPlaced = false;
+        this.moveSubmitted = false;
+    }
     @Override
     public void run() {
         try (
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
         ) {
+            this.out = out;
             String inputLine;
+
+            out.println("Player id " + playerId);
+
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Server received: " + inputLine);
 
-                if (inputLine.equals("stop")) {
+                if (inputLine.equals("stop") || inputLine.equals("exit")) {
                     out.println("Server stopped");
                     gameServer.stop();
                     break;
-                } else {
+                } else if(inputLine.equalsIgnoreCase("create game") && gameServer.getCurrentState() == GameState.GAME_NOT_CREATED){
+                    gameServer.addWaitingPlayer(this);
+                    gameServer.setCurrentState(GameState.WAITING_FOR_PLAYER);
+                    //starea de incepere a jocului
+
+                    startGame();
+                    joinGame(in);
+//                    if(opponent == null){
+//                        sendMessage("Waiting for another player to join ...");
+//                    }
+
+                } else if(inputLine.equalsIgnoreCase("join game")){
+                    if(gameServer.getCurrentState() == GameState.WAITING_FOR_PLAYER) {
+
+                        startGame();
+                        joinGame(in);
+
+                        //gameServer.setCurrentState(GameState.PLAYER1_TURN);
+                        // playerTurn = GameState.PLAYER1_TURN; //punem randul playerului 1
+
+                    }else {
+                        sendMessage("Game isn't created.");
+                    }
+                } else if(gameServer.getCurrentState() == GameState.GAME_READY_TO_MOVE/*inputLine.startsWith("submit move")*/){
+
+                        if(playerId == playerTurn.getStateCode()) {
+                            submitMove(inputLine);
+                            System.out.println("Player " + playerId + " moved " + inputLine);
+                            switchTurn();
+                        }else{
+                            sendMessage("It's not your turn.");
+                        }
+                } else if(gameServer.getCurrentState() == GameState.GAME_OVER) {
+                    //jocul s a terminat.
+                       gameIsFinished();
+                }else {
                     out.println("Server received the request: " + inputLine);
                 }
             }
         } catch (IOException e) {
+            handleDisconnection(e);
+        }  finally {
+            closeClientSocket();
+
+        }
+    }
+
+    private void joinGame(BufferedReader in ) throws IOException{
+        sendMessage("Game started. PLace your ships.");
+
+        waititngPlayersForJoining();
+
+        gameServer.setCurrentState(GameState.GAME_READY_TO_MOVE);
+        //placeShip(in, "Carrier", CARRIER_LENGTH);
+       // placeShip(in, "Battleship", BATTLESHIP_LENGTH);
+       // placeShip(in, "Destroyer", DESTROYER_LENGTH);
+        //placeShip(in, "Submarine", SUBMARINE_LENGTH);
+        placeShip(in, "Patrol Boat", PATROL_BOAT_LENGTH);
+        shipsPlaced = true;
+
+        //setam daca playerul este ready
+        makePlayerReady();
+
+        checkReadyToStart();
+    }
+    private void placeShip(BufferedReader in, String shipName, int length) throws IOException{
+        sendMessage("Place your " + shipName + " (length = " + length + "): ");
+        int placed;
+        do {
+            try {
+
+                String inputLine = in.readLine();
+                placed = gameServer.placeShip(playerId, inputLine, length);
+
+            } catch (GameException e ){
+                placed = -1; // ca sa ramana in while
+
+                sendMessage(e.getMessage());
+
+            }
+        } while (placed < 0);
+
+    }
+
+    private void submitMove(String inputLine) {
+       if(isReadyToMove()){
+            String move = inputLine.trim();
+            gameServer.handleMove(playerId, move);
+            sendMessage("Move submitted: " + move + ". Waiting for opponent's move.");
+            //opponent.sendMessage("Opponent moved: " + move + ". Your turn.");
+        }
+    }
+    private boolean isReadyToMove(){
+        if ( gameServer.getCurrentState() == GameState.GAME_READY_TO_MOVE && playerId != playerTurn.getStateCode()) {
+            sendMessage("It's not your turn or game is not ready yet.");
+            return false;
+        }else {
+            return true;
+        }
+    }
+    private synchronized void checkReadyToStart() {
+//        if (shipsPlaced && opponent != null && opponent.shipsPlaced) {
+//            sendMessage("Both players have placed their ships. Player 1 starts. Type 'submit move <position>' to play.");
+//            if (playerId == 1) {
+//                sendMessage("Your turn.");
+//                opponent.sendMessage("Waiting for Player 1 to move.");
+//            } else {
+//                sendMessage("Waiting for Player 1 to move.");
+//                opponent.sendMessage("Your turn.");
+//            }
+//        } else if (opponent != null && !opponent.shipsPlaced) {
+//            sendMessage("Waiting for opponent to place ships...");
+//        }
+        if(gameServer.isPlayer1IsReady() && gameServer.isPlayer2IsReady()) {
+            sendMessage(" PlaBoth players have placed their ships.yer 1 starts.");
+            //opponent.sendMessage("PlaBoth players have placed their ships.yer 1 starts.");
+            gameServer.setCurrentState(GameState.GAME_READY_TO_MOVE);
+            playerTurn = GameState.PLAYER1_TURN;
+//            if (playerId == 1) {
+//                sendMessage("Your turn.");
+//                opponent.sendMessage("Waiting for Player 1 to move.");
+//            } else {
+//                sendMessage("Waiting for Player 1 to move.");
+//                opponent.sendMessage("Your turn.");
+//            }
+
+        }else{
+            sendMessage("Waiting for opponent to place ships...");
+            while( gameServer.getCurrentState() != GameState.GAME_READY_TO_MOVE){
+               waitingThread();
+            }
+        }
+    }
+    private void makePlayerReady(){
+        if(playerId == 1)  {
+            gameServer.setPlayer1IsReady(true);
+        }else{
+            gameServer.setPlayer2IsReady(true);
+        }
+    }
+    private void waititngPlayersForJoining(){
+        if (playerId == 1){
+            gameServer.setPlayer1IsReady(true);
+            while(!gameServer.isPlayer2IsReady()){
+                waitingThread();
+            }
+        }else{
+            gameServer.setPlayer2IsReady(true);
+            while(!gameServer.isPlayer1IsReady()){
+                waitingThread();
+            }
+        }
+    }
+    private void waitingThread(){
+        try {
+            Thread.sleep(1000);
+            sendMessage("wait");
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    private void switchTurn() {
+        if(playerTurn == GameState.PLAYER1_TURN) {
+            playerTurn = GameState.PLAYER2_TURN;
+        }else {
+
+            playerTurn = GameState.PLAYER1_TURN;
+        }
+    }
+    private void sendMessage(String message) {
+        if (out != null) {
+            out.println(message);
+        }
+    }
+    private void handleDisconnection(IOException e) {
+        System.out.println("Client disconnected abruptly: " + e.getMessage());
+//        if (opponent != null) {
+//            opponent.sendMessage("Opponent disconnected. Waiting for another player...");
+//            //opponent.setOpponent(null);
+//            //gameServer.addWaitingPlayer(opponent);
+//        }
+    }
+    private void closeClientSocket() {
+        try {
+            gameServer.playerLeft(this);
+            clientSocket.close();
+
+        } catch (IOException e) {
+            System.out.println("Error when closing client socket: " + e.getMessage());
+        }
+    }
+    public void notifyHit(String move) {
+        sendMessage("You hit at position: " + move);
+    }
+    public void notifyMiss(String move) {
+        sendMessage("You missed at position: " + move);
+    }
+//    public void setOpponent(ClientThread opponent) {
+//        this.opponent = opponent;
+//        if (opponent != null) {
+//            this.playerId = 1;
+//            opponent.setPlayerId(2);
+//        }
+//    }
+    public void setPlayerId(int id) {
+        this.playerId = id;
+    }
+    public ClientThread getOpponent() {
+        return opponent;
+    }
+    public void startGame() {
+        sendMessage("1-Both players connected. Type 'join game' to start.");
+//        if (opponent != null) {
+//            opponent.sendMessage("2-Both players connected. Type 'join game' to start.");
+//        }
+
+    }
+
+    private void gameIsFinished(){
+        if(playerTurn == GameState.PLAYER1_TURN){
+            gameServer.displayServerBoard();
+            gameServer.setCurrentState(GameState.GAME_NOT_CREATED);
+        }else{
+            gameServer.displayServerBoard();
+            gameServer.setCurrentState(GameState.GAME_NOT_CREATED);
+        }
+    }
+    public void notifyGameOver() {
+        sendMessage("Game over. You won!");
+//        if (opponent != null) {
+//            opponent.sendMessage("Game over. You lost!");
+//        }
     }
 }
--- Lab10/ServerApplication/GameServer.java
+++ Lab10/ServerApplication/GameServer.java
@@ -1,20 +1,212 @@
 package org.example;
 
+import org.example.Exception.GameException;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
+//import lombok.Getter;
+//
+//@Getter
+//@Setter
 public class GameServer {
+    //constante
+    public static final int BOARD_SIZE = 10;
     private int port;
     private volatile boolean isRunning;
     private ServerSocket serverSocket;
 
+    private LinkedList<ClientThread> waitingPlayers;
+    private char[][] serverBoardPlayer1;
+    private char[][] serverBoardPlayer2;
+
+    private List<Thread> clientThreads;
+
+
+    private AtomicInteger numberOfPlayers ;
+
+    private GameState currentState;
+    private boolean player1IsReady;
+    private boolean player2IsReady;
+
+    private int player1TotalShips;
+    private int player2TotalShips;
+
     public GameServer(int port) {
         this.port = port;
         this.isRunning = false;
+
+        clientThreads = new ArrayList<>();
+        numberOfPlayers = new AtomicInteger(0);
+
+        currentState = GameState.GAME_NOT_CREATED;
+        player1IsReady=false;
+        player2IsReady=false;
+
+        waitingPlayers = new LinkedList<>();
+        this.serverBoardPlayer1 = new char[BOARD_SIZE][BOARD_SIZE];
+        this.serverBoardPlayer2 = new char[BOARD_SIZE][BOARD_SIZE];
+        initializeBoard(serverBoardPlayer1);
+        initializeBoard(serverBoardPlayer2);
+
+        //setam sizeul la 1 pt a testa mai usor
+        player1TotalShips = 1;
+        player2TotalShips = 1;
     }
 
+    private void initializeBoard(char[][] board) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = '.';
+            }
+        }
+    }
+
+    private boolean checkShipAlive( char[][] board,int lineMove,int columnMove) {
+
+
+        if (board[lineMove][columnMove - 1] == 'X' || board[lineMove][columnMove - 1] == '.') {
+            if (board[lineMove][columnMove + 1] == 'X' || board[lineMove][columnMove + 1] == '.') {
+                if (board[lineMove - 1][columnMove] == 'X' || board[lineMove - 1][columnMove] == '.') {
+                    if (board[lineMove + 1][columnMove] == 'X' || board[lineMove + 1][columnMove] == '.') {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public synchronized void handleMove(int playerId, String move) {
+        int row = move.charAt(0) - 'A'; //convertesc prima litera pt a obtina linia
+        int col = Integer.parseInt(move.substring(1)) - 1; //convertesc a 2 a litera pt a obtine coloana
+
+
+        char[][] board = playerId == 1 ? serverBoardPlayer2 : serverBoardPlayer1; //vad care board trebuie actualizat
+
+        if (board[row][col] == '#' || board[row][col] == 'o') {
+            board[row][col] = 'X';
+            System.out.println("Player " + playerId + " hit at position: " + move);
+            ClientThread player = playerId == 1 ? waitingPlayers.getFirst() : waitingPlayers.getLast();
+            player.notifyHit(move);
+            //player.getOpponent().notifyHit(move);
+            if(checkShipAlive(board,row,col)){
+                if(playerId == 1){
+                    player1TotalShips--;
+                    if(player1TotalShips == 0){
+                        player.notifyGameOver();
+                        //player.getOpponent().notifyGameOver();
+                        currentState = GameState.GAME_OVER;
+                    }
+                }else{
+                    player2TotalShips--;
+                    if(player2TotalShips == 0){
+                        player.notifyGameOver();
+                        //player.getOpponent().notifyGameOver();
+                        currentState = GameState.GAME_OVER;
+                    }
+                }
+            }
+        } else {
+            board[row][col] = '?';
+            System.out.println("Player " + playerId + " missed at position: " + move);
+            ClientThread player = playerId == 1 ? waitingPlayers.getFirst() : waitingPlayers.getLast();
+
+            //notificam miscarile
+            player.notifyMiss(move);
+            //player.getOpponent().notifyMiss(move);
+        }
+
+        displayServerBoard();
+    }
+
+
+
+    public synchronized void addWaitingPlayer(ClientThread player) {
+        waitingPlayers.add(player);
+        if (waitingPlayers.size() == 2) {
+            ClientThread player1 = waitingPlayers.removeFirst();
+            ClientThread player2 = waitingPlayers.removeFirst();
+//             player1.setOpponent(player2);
+//            player2.setOpponent(player1);
+            player1.startGame();
+        }
+    }
+
+    private void validateShipPositions(char[][] board, int[] rows, int[] cols) throws GameException{
+        for (int i = 0; i < rows.length; i++) {
+            if (board[rows[i]][cols[i]] != '.') {
+                throw new GameException(GameException.ErrorCode.POSITIONS_NOT_EMPTY);
+            }
+            if (i > 0) {
+                if (!((rows[i] == rows[i - 1] && Math.abs(cols[i] - cols[i - 1]) == 1) || (cols[i] == cols[i - 1] && Math.abs(rows[i] - rows[i - 1]) == 1))) {
+                    throw new GameException(GameException.ErrorCode.POSITIONS_NOT_CONSECUTIVE);
+                }
+            }
+        }
+    }
+    private void placeShipOnBoard(char[][] board, int[] rows, int[] cols) {
+        for (int i = 0; i < rows.length; i++) {
+            if (i == 0 || i == rows.length - 1) {
+                board[rows[i]][cols[i]] = '#'; // First and last positions of the ship
+            } else {
+                board[rows[i]][cols[i]] = 'o'; // Intermediate positions of the ship
+            }
+        }
+    }
+    public synchronized int placeShip(int playerId, String input, int length) throws GameException{
+        char[][] board = playerId == 1 ? serverBoardPlayer1 : serverBoardPlayer2;
+        String[] positions = input.split(" ");
+        if (positions.length != length) {
+            System.out.println("Incorect pos ");
+            throw new GameException(GameException.ErrorCode.INCORRECT_POSITIONS);
+        }
+
+        int[] rows = new int[length];
+        int[] cols = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            try {
+                rows[i] = positions[i].charAt(0) - 'A';
+                cols[i] = Integer.parseInt(positions[i].substring(1)) - 1;
+            } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                System.out.println("Incorect pos format ");
+                throw new GameException(GameException.ErrorCode.INVALID_POSITION_FORMAT);
+            }
+        }
+
+        validateShipPositions(board, rows, cols);
+        placeShipOnBoard(board, rows, cols);
+
+        displayServerBoard();
+        return 0;
+    }
+
+
+    public void displayServerBoard() {
+        System.out.println("Server Board Player 1:");
+        displayBoard(serverBoardPlayer1);
+        System.out.println("Server Board Player 2:");
+        displayBoard(serverBoardPlayer2);
+    }
+    private void displayBoard(char[][] board) {
+        System.out.println("      1 2 3 4 5 6 7 8 9 BOARD_SIZE");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.print((char)('A' + i) + "    ");
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public void start() {
         try {
             serverSocket = new ServerSocket(port);
@@ -23,9 +215,30 @@ public class GameServer {
 
             while (isRunning) {
                 try {
-                    Socket clientSocket = serverSocket.accept();
-                    System.out.println("New client connected");
-                    new ClientThread(clientSocket, this).start();
+                    if(clientThreads.size() < 2) {
+                        Socket clientSocket = serverSocket.accept();
+                        System.out.println("New client connected");
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        out.println("Connection Completed");
+
+                        numberOfPlayers.incrementAndGet();
+                        Thread client = new ClientThread(clientSocket, this);
+
+                        client.start();
+                        clientThreads.add(client);
+
+
+                    }else{
+                        Socket clientSocket = serverSocket.accept();
+                        System.out.println("Cannot connect");
+
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        out.println("-10");//Server is Full
+                        clientSocket.close();
+                    }
+
+
+
                 } catch (SocketException e) {
                     if (isRunning) {
                         System.out.println("Error in client connection");
@@ -54,6 +267,7 @@ public class GameServer {
     public void stop() {
         isRunning = false;
         System.out.println("Server stopped.");
+
         try {
             if (serverSocket != null) {
                 serverSocket.close();
@@ -64,8 +278,38 @@ public class GameServer {
         }
     }
 
-    public boolean isRunning() {
-        return isRunning;
+
+    public void playerLeft(Thread t){
+        clientThreads.remove(t);
+        numberOfPlayers.decrementAndGet();
+    }
+
+    public AtomicInteger getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public GameState getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(GameState currentState) {
+        this.currentState = currentState;
+    }
+
+    public boolean isPlayer2IsReady() {
+        return player2IsReady;
+    }
+
+    public boolean isPlayer1IsReady() {
+        return player1IsReady;
+    }
+
+    public void setPlayer2IsReady(boolean player2IsReady) {
+        this.player2IsReady = player2IsReady;
+    }
+
+    public void setPlayer1IsReady(boolean player1IsReady) {
+        this.player1IsReady = player1IsReady;
     }
 
     public static void main(String[] args) {
